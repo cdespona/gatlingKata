ab4c7633f2b2280272a470d836c1f8896edeb2c9
Carles Espona <carles.espona@mango.com>
2020-03-05 10:14:19 +0100

    Rename simulation as it is using a reserved type

---
+++ src/gatling/simulations/com/mango/SpecificSimulation.scala
@@ -0,0 +1,20 @@
+package com.mango
+
+import com.mango.configurations.Configuration._
+import com.mango.scenarios.Scenario
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class SpecificSimulation extends Simulation {
+  val httpConf: HttpProtocolBuilder = http
+    .baseUrl(baseUrl)
+    .acceptHeader(accept)
+    .doNotTrackHeader(doNotTrack)
+    .acceptLanguageHeader(acceptLanguage)
+    .acceptEncodingHeader(acceptEncoding)
+    .userAgentHeader(userAgent)
+}
